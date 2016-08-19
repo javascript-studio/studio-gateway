702b63cedd2c4531a3fb62a49c892bb83a8c11f1
Maximilian Antoni <mail@maxantoni.de>
2016-08-19 22:45:50 +0200

    Inception

---
+++ README.md
@@ -0,0 +1,29 @@
+# JavaScript Studio Gateway
+
+A custom Swagger interpreter with AWS Lambda integration support, for local API
+Gateway testing.
+
+## Usage
+
+Assuming a `swagger.json` file in the current directory:
+
+```js
+const gateway = require('@studio/gateway');
+const lambda = require('@studio/lambda');
+
+const lambda_ctrl = lambda.create();
+const gateway_server = gateway.create(lambda_ctrl)
+gateway_server.on('lambda', lambda_ctrl.invoke);
+gateway_server.listen(1337);
+```
+
+## API
+
+- `gateway_server = gateway.create([options])`: Returns a new gateway server
+  for the given options.
+    - `swagger_file`: The swagger file to read. Defaults to `swagger.json`.
+- `gateway_server.listen(port[, callback])`: Bind the server to the given port.
+
+## Events
+
+- `lambda(name, event, callback)`: When a lambda integration should be invoked.
---
+++ test/gateway-test.js
@@ -0,0 +1,359 @@
+/*eslint-env mocha*/
+'use strict';
+
+const fs = require('fs');
+const supertest = require('supertest');
+const sinon = require('sinon');
+const gateway = require('..');
+
+describe('gateway', () => {
+  let sandbox;
+  let swagger;
+  let server;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    swagger = sandbox.stub(fs, 'readFileSync').withArgs('swagger.json');
+  });
+
+  afterEach((done) => {
+    sandbox.restore();
+    if (server) {
+      server.close(done);
+      server = null;
+    } else {
+      done();
+    }
+  });
+
+  function create(options) {
+    server = gateway.create(options);
+    server.listen();
+    return server;
+  }
+
+  function swag(json) {
+    swagger.returns(JSON.stringify(json));
+  }
+
+  it('returns 404 for unknown path', (done) => {
+    swag({ paths: {} });
+
+    supertest(create())
+      .get('/unknown')
+      .expect(404, done);
+  });
+
+  it('returns 404 for unknown method', (done) => {
+    swag({
+      paths: {
+        '/foo': { post: {} }
+      }
+    });
+
+    supertest(create())
+      .get('/foo')
+      .expect(404, done);
+  });
+
+  it('returns 404 if aws integration is missing', (done) => {
+    swag({
+      paths: {
+        '/foo': { get: {} }
+      }
+    });
+
+    supertest(create())
+      .get('/foo')
+      .expect(404, done);
+  });
+
+  it('responds with mock response', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          get: {
+            responses: { 200: {} },
+            'x-amazon-apigateway-integration': {
+              type: 'mock',
+              responses: {
+                default: {
+                  statusCode: '200',
+                  responseParameters: {
+                    'method.response.header.x-foo-bar': '"test"'
+                  },
+                  responseTemplate: '{}'
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+
+    supertest(create())
+      .get('/foo')
+      .expect('content-type', 'application/json')
+      .expect('x-foo-bar', 'test')
+      .expect(200, done);
+  });
+
+  it('replaces __ORIGIN__ with request origin', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          get: {
+            responses: { 200: {} },
+            'x-amazon-apigateway-integration': {
+              type: 'mock',
+              responses: {
+                default: {
+                  statusCode: '200',
+                  responseParameters: {
+                    'method.response.header.x-foo-bar': '"__ORIGIN__"'
+                  },
+                  responseTemplate: '{}'
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+
+    supertest(create())
+      .get('/foo')
+      .set('origin', 'http://javascript.studio')
+      .expect('x-foo-bar', 'http://javascript.studio')
+      .expect(200, done);
+  });
+
+  it('emits "lambda" event with empty body', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          post: {
+            responses: { 200: {} },
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    const stub = sinon.stub().yields(null, { some: 'response' });
+    server.on('lambda', stub);
+
+    supertest(server)
+      .post('/foo')
+      .set('accept', 'application/json')
+      .send({ some: 'request' }) // ignored
+      .expect('{"some":"response"}')
+      .expect(200, (err) => {
+        if (err) {
+          throw err;
+        }
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'some-lambda', {}); // empty body
+        done();
+      });
+  });
+
+  it('checks for regexp match to figure out response to use', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          post: {
+            responses: { 200: {} },
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                '.*"code":"E_*': { statusCode: '500' },
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    server.on('lambda', sinon.stub().yields('{"code":"E_FOO"}'));
+
+    supertest(server)
+      .post('/foo')
+      .expect('{"code":"E_FOO"}')
+      .expect(500, done);
+  });
+
+  it('maps request header to parameter', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          post: {
+            responses: { 200: {} },
+            parameters: [{
+              name: 'Authorization',
+              in: 'header',
+              type: 'string'
+            }],
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    const stub = sinon.stub().yields(null, {});
+    server.on('lambda', stub);
+
+    supertest(server)
+      .post('/foo')
+      .set('Authorization', 'Secret')
+      .expect(200, (err) => {
+        if (err) {
+          throw err;
+        }
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'some-lambda', {
+          Authorization: 'Secret'
+        });
+        done();
+      });
+  });
+
+  it('maps body json to parameter', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          post: {
+            responses: { 200: {} },
+            parameters: [{
+              name: 'some',
+              in: 'body',
+              type: 'string'
+            }],
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    const stub = sinon.stub().yields(null, {});
+    server.on('lambda', stub);
+
+    supertest(server)
+      .post('/foo')
+      .set('accept', 'application/json')
+      .send({ some: 'content' })
+      .expect(200, (err) => {
+        if (err) {
+          throw err;
+        }
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'some-lambda', {
+          some: 'content'
+        });
+        done();
+      });
+  });
+
+  it('maps query to parameter', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          get: {
+            responses: { 200: {} },
+            parameters: [{
+              name: 'some',
+              in: 'query',
+              type: 'string'
+            }],
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    const stub = sinon.stub().yields(null, {});
+    server.on('lambda', stub);
+
+    supertest(server)
+      .get('/foo?some=query')
+      .expect(200, (err) => {
+        if (err) {
+          throw err;
+        }
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'some-lambda', {
+          some: 'query'
+        });
+        done();
+      });
+  });
+
+  it('maps non-json body to parameter', (done) => {
+    swag({
+      paths: {
+        '/foo': {
+          post: {
+            responses: { 200: {} },
+            parameters: [{
+              name: 'some',
+              in: 'body',
+              type: 'string'
+            }],
+            'x-amazon-apigateway-integration': {
+              type: 'aws',
+              uri: '__APIGATEWAY__/__LAMBDA__some-lambda:current/invocations',
+              responses: {
+                default: { statusCode: '200' }
+              }
+            }
+          }
+        }
+      }
+    });
+    create();
+    const stub = sinon.stub().yields(null, {});
+    server.on('lambda', stub);
+
+    supertest(server)
+      .post('/foo')
+      .send('some=content')
+      .expect(200, (err) => {
+        if (err) {
+          throw err;
+        }
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWith(stub, 'some-lambda', {
+          some: 'content'
+        });
+        done();
+      });
+
+  });
+
+});
